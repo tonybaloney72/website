26009d8cf65152f56b242703e5490f9edba3095c
Anthony Bologna <bologna.anthony@gmail.com>
2025-11-10 15:12:12 -0800

    center links, add routing, create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { NavBar } from "./components/Nav";
 import { ThemeProvider } from "./context/themeProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { HomePage } from "./pages/Home";
+import { AboutPage } from "./pages/About";
+import { ProjectPage } from "./pages/Projects";
+import { ContactPage } from "./pages/Contact";
 
 function App() {
 	return (
-		<ThemeProvider>
-			<div className='bg-gray-200 dark:bg-gray-800 min-h-screen'>
-				<NavBar />
-				<div></div>
-			</div>
-		</ThemeProvider>
+		<BrowserRouter>
+			<ThemeProvider>
+				<div className='bg-gray-200 dark:bg-gray-800 min-h-screen'>
+					<NavBar />
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/about' element={<AboutPage />} />
+						<Route path='/projects' element={<ProjectPage />} />
+						<Route path='/contact' element={<ContactPage />} />
+					</Routes>
+				</div>
+			</ThemeProvider>
+		</BrowserRouter>
 	);
 }
 
 export default App;
-//test
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,20 @@
 import { useTheme } from "../context/themeProvider";
 import { FaMoon } from "react-icons/fa";
 import { MdWbSunny } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 export const NavBar = () => (
-	<div className='flex justify-center items-center gap-8 h-16 px-6'>
-		<NavLink name='Home' />
-		<NavLink name='About' />
-		<NavLink name='Projects' />
-		<NavLink name='Contact' />
-		<DarkToggle />
+	<div className='flex items-center h-16 px-6 w-full'>
+		<div className='flex-1'></div>
+		<div className='flex justify-center items-center gap-8 flex-1'>
+			<NavLink name='Home' />
+			<NavLink name='About' />
+			<NavLink name='Projects' />
+			<NavLink name='Contact' />
+		</div>
+		<div className='flex-1 flex justify-end'>
+			<DarkToggle />
+		</div>
 	</div>
 );
 
@@ -16,11 +22,31 @@ interface NavLinkProps {
 	name: string;
 }
 
-const NavLink = ({ name }: NavLinkProps) => (
-	<button className='text-2xl text-gray-800 dark:text-gray-200 hover:cursor-pointer'>
-		<span className='underline-animation'>{name}</span>
-	</button>
-);
+const NAVMAP = {
+	Home: "/",
+	About: "/about",
+	Projects: "/projects",
+	Contact: "/contact",
+};
+
+const NavLink = ({ name }: NavLinkProps) => {
+	const nav = useNavigate();
+
+	const handleClick = () => {
+		const route = NAVMAP[name as keyof typeof NAVMAP];
+		if (route) {
+			nav(route);
+		}
+	};
+
+	return (
+		<button
+			className='text-3xl text-gray-800 dark:text-gray-200 hover:cursor-pointer'
+			onClick={handleClick}>
+			<span className='underline-animation'>{name}</span>
+		</button>
+	);
+};
 
 const MoonIcon = () => (
 	<FaMoon className='text-gray-800 hover:cursor-pointer hover:transition-colors duration-300 hover:text-gray-500' />
